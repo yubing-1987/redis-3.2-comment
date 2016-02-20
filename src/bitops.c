f3b5b7a3246081720815cf4740fbef2eae010c86
yubing <ybjx1987@qq.com>
2016-02-20 17:16:55 +0800

    1、完成了redis位运算相关的模块注释

--- src/bitops.c
+++ src/bitops.c
@@ -155,16 +155,22 @@ long redisBitpos(void *s, unsigned long count, int bit) {
      * aligned. */
 
     /* Skip initial bits not aligned to sizeof(unsigned long) byte by byte. */
+    //当[bit]==[0]的时候，如果一个byte=-1，那就表面这个byte里面没有哪一个bit为[0]，就需要跳过
+    //同理，[bit]==[1]的时候，byte==0也是需要跳过的
     skipval = bit ? 0 : UCHAR_MAX;
     c = (unsigned char*) s;
+    //这里主要是为了进行内存地址对齐的
     while((unsigned long)c & (sizeof(*l)-1) && count) {
-        if (*c != skipval) break;
+        if (*c != skipval) break;//走到了break表示找到相应的值
+        //没有符合的bit，跳到下一个byte
         c++;
         count--;
         pos += 8;
     }
 
     /* Skip bits with full word step. */
+    //这个和上面是一样的道理，由于上一步已经进行了内存对齐，
+    //所以，可以直接取一个long的值进行校验，这样会快很多
     skipval = bit ? 0 : ULONG_MAX;
     l = (unsigned long*) c;
     while (count >= sizeof(*l)) {
@@ -181,10 +187,13 @@ long redisBitpos(void *s, unsigned long count, int bit) {
      *
      * Note that the loading is designed to work even when the bytes left
      * (count) are less than a full word. We pad it with zero on the right. */
+    //这里处理剩余的byte，主要是把剩余的byte组合到一个long里面
     c = (unsigned char*)l;
     for (j = 0; j < sizeof(*l); j++) {
+        //右移8bit
         word <<= 8;
         if (count) {
+            //把byte的值复制到word里面
             word |= *c;
             c++;
             count--;
@@ -196,17 +205,26 @@ long redisBitpos(void *s, unsigned long count, int bit) {
      * return -1 to signal that there is not a single "1" in the whole
      * string. This can't happen when we are looking for "0" as we assume
      * that the right of the string is zero padded. */
+    //如果想找到bit为[1]的，结果到这里[word]=0，那就是表示没有找到，返回[-1]
     if (bit == 1 && word == 0) return -1;
 
     /* Last word left, scan bit by bit. The first thing we need is to
      * have a single "1" set in the most significant position in an
      * unsigned long. We don't know the size of the long so we use a
      * simple trick. */
+    //全部bit设置为[1]
     one = ULONG_MAX; /* All bits set to 1.*/
+    //右移一位，最高位设置为[0]，其它全部是[1]
     one >>= 1;       /* All bits set to 1 but the MSB. */
+    //取反，最高位为[1]，其它全是[0]
     one = ~one;      /* All bits set to 0 but the MSB. */
 
     while(one) {
+        //从高到低探测bit是不是为1
+        //（one & word）!= 0    如果指定位为[0] 则为[false]->[0]
+        //如果指定位为[1] 则为[true]->[1]
+        //如果bit == [1] 对应的是 （one & word）!= 0为真
+        //如果bit == [0] 对应的是 （one & word）!= 0为假
         if (((one & word) != 0) == bit) return pos;
         pos++;
         one >>= 1;
@@ -215,6 +233,7 @@ long redisBitpos(void *s, unsigned long count, int bit) {
     /* If we reached this point, there is a bug in the algorithm, since
      * the case of no match is handled as a special case before. */
     serverPanic("End of redisBitpos() reached.");
+    //走到这里一般是不应该的
     return 0; /* Just to avoid warnings. */
 }
 
@@ -228,6 +247,9 @@ long redisBitpos(void *s, unsigned long count, int bit) {
 #define BITOP_NOT   3
 
 /* SETBIT key offset bitvalue */
+/*
+ * 设置指定偏移的bit值
+ */
 void setbitCommand(client *c) {
     robj *o;
     char *err = "bit is not an integer or out of range";
@@ -235,73 +257,98 @@ void setbitCommand(client *c) {
     int byte, bit;
     int byteval, bitval;
     long on;
-
+    //得到偏移
     if (getBitOffsetFromArgument(c,c->argv[2],&bitoffset) != C_OK)
         return;
-
+    //得到需要设置的bit值
     if (getLongFromObjectOrReply(c,c->argv[3],&on,err) != C_OK)
         return;
 
     /* Bits can only be set or cleared... */
+    //bit的值只能够是[0]和[1]
     if (on & ~1) {
         addReplyError(c,err);
         return;
     }
-
+    //计算byte的偏移
     byte = bitoffset >> 3;
+    //得到redis数据
     o = lookupKeyWrite(c->db,c->argv[1]);
     if (o == NULL) {
+        //如果原来不存在，就创建一个，并初始设置为空字符串，长度为刚好容下位偏移
         o = createObject(OBJ_STRING,sdsnewlen(NULL, byte+1));
+        //添加到数据库
         dbAdd(c->db,c->argv[1],o);
     } else {
+        //只有是字符串类型才可以进行位计算
         if (checkType(c,o,OBJ_STRING)) return;
+        //拷贝一个不会被其它地方使用的redis数据
         o = dbUnshareStringValue(c->db,c->argv[1],o);
+        //如果字符串的长度小于[byte+1]，就进行扩容，用[0]进行填充
         o->ptr = sdsgrowzero(o->ptr,byte+1);
     }
 
     /* Get current values */
+    //得到当前的btye值
     byteval = ((uint8_t*)o->ptr)[byte];
+    //得到当前bit值
     bit = 7 - (bitoffset & 0x7);
+    //计算指定位置的bit值
     bitval = byteval & (1 << bit);
 
     /* Update byte with new bit value and return original value */
+    //指定位置的值设置为[0]
     byteval &= ~(1 << bit);
+    //计算出指定位置新的值
     byteval |= ((on & 0x1) << bit);
+    //更新
     ((uint8_t*)o->ptr)[byte] = byteval;
     signalModifiedKey(c->db,c->argv[1]);
     notifyKeyspaceEvent(NOTIFY_STRING,"setbit",c->argv[1],c->db->id);
     server.dirty++;
+    //返回原来指定位置的bit值
     addReply(c, bitval ? shared.cone : shared.czero);
 }
 
 /* GETBIT key offset */
+/*
+ * GETBIT命令，用来得到redis数据指定位置的bit值
+ */
 void getbitCommand(client *c) {
     robj *o;
     char llbuf[32];
     size_t bitoffset;
     size_t byte, bit;
     size_t bitval = 0;
-
+    //得到位偏移
     if (getBitOffsetFromArgument(c,c->argv[2],&bitoffset) != C_OK)
         return;
-
+    //查找redis数据
     if ((o = lookupKeyReadOrReply(c,c->argv[1],shared.czero)) == NULL ||
         checkType(c,o,OBJ_STRING)) return;
-
+    //计算byte的偏移
     byte = bitoffset >> 3;
+    //计算在最后一个byte中的bit偏移
     bit = 7 - (bitoffset & 0x7);
     if (sdsEncodedObject(o)) {
+        //普通的字符串
         if (byte < sdslen(o->ptr))
+            //如果没有超出最大长度，直接就可以计算bit的值
             bitval = ((uint8_t*)o->ptr)[byte] & (1 << bit);
     } else {
+        //数字表示的字符串，需要先转换成真的字符串
         if (byte < (size_t)ll2string(llbuf,sizeof(llbuf),(long)o->ptr))
+            //计算bit值
             bitval = llbuf[byte] & (1 << bit);
     }
-
+    //返回得到的bit值，如果越界就返回[0]
     addReply(c, bitval ? shared.cone : shared.czero);
 }
 
 /* BITOP op_name target_key src_key1 src_key2 src_key3 ... src_keyN */
+/*
+ * BITOP操作，位与，位或，取反，异或等四种操作
+ */
 void bitopCommand(client *c) {
     char *opname = c->argv[1]->ptr;
     robj *o, *targetkey = c->argv[2];
@@ -314,6 +361,9 @@ void bitopCommand(client *c) {
     unsigned char *res = NULL; /* Resulting string. */
 
     /* Parse the operation name. */
+    //校验操作名称
+    //[and]---与操作，[or]---或操作，[xor]---异或操作，[not]---非操作
+    //操作名称不区分大小写
     if ((opname[0] == 'a' || opname[0] == 'A') && !strcasecmp(opname,"and"))
         op = BITOP_AND;
     else if((opname[0] == 'o' || opname[0] == 'O') && !strcasecmp(opname,"or"))
@@ -328,20 +378,27 @@ void bitopCommand(client *c) {
     }
 
     /* Sanity check: NOT accepts only a single key argument. */
+    //对于非操作，只能够支持一个被操作对象
     if (op == BITOP_NOT && c->argc != 4) {
         addReplyError(c,"BITOP NOT must be called with a single source key.");
         return;
     }
 
     /* Lookup keys, and store pointers to the string objects into an array. */
+    //被操作对象的个数
     numkeys = c->argc - 3;
+    //存放源数据字符串指针
     src = zmalloc(sizeof(unsigned char*) * numkeys);
+    //存放源数据字符串长度
     len = zmalloc(sizeof(long) * numkeys);
+    //存放存放源数据
     objects = zmalloc(sizeof(robj*) * numkeys);
     for (j = 0; j < numkeys; j++) {
+        //获取redis数据
         o = lookupKeyRead(c->db,c->argv[j+3]);
         /* Handle non-existing keys as empty strings. */
         if (o == NULL) {
+            //redis数据没有找到，就直接跳过
             objects[j] = NULL;
             src[j] = NULL;
             len[j] = 0;
@@ -350,25 +407,32 @@ void bitopCommand(client *c) {
         }
         /* Return an error if one of the keys is not a string. */
         if (checkType(c,o,OBJ_STRING)) {
+            //如果其中任意一个不是字符串类型的，就返回错误
             unsigned long i;
             for (i = 0; i < j; i++) {
                 if (objects[i])
                     decrRefCount(objects[i]);
             }
+            //释放资源
             zfree(src);
             zfree(len);
             zfree(objects);
             return;
         }
+        //获取一个解码过的redis数据
         objects[j] = getDecodedObject(o);
+        //源数据字符串指针
         src[j] = objects[j]->ptr;
+        //源数据字符串长度
         len[j] = sdslen(objects[j]->ptr);
+        //计算出全部字符串中最长的长度
         if (len[j] > maxlen) maxlen = len[j];
+        //计算出全部字符串中最短的长度
         if (j == 0 || len[j] < minlen) minlen = len[j];
     }
 
     /* Compute the bit operation, if at least one string is not empty. */
-    if (maxlen) {
+    if (maxlen) {//[maxlen]不等于[0]表面至少有一个字符串长度不为空
         res = (unsigned char*) sdsnewlen(NULL,maxlen);
         unsigned char output, byte;
         unsigned long i;
@@ -378,16 +442,23 @@ void bitopCommand(client *c) {
          * vanilla algorithm. */
         j = 0;
         if (minlen >= sizeof(unsigned long)*4 && numkeys <= 16) {
+            //只对全部字符串长度都大于4个[unsigned long]的进行处理
+            //如果有一个字符串已经处理完了，这里也不需要进来
+            //这里一次会处理4个[unsigned long]的数据
             unsigned long *lp[16];
             unsigned long *lres = (unsigned long*) res;
 
             /* Note: sds pointer is always aligned to 8 byte boundary. */
             memcpy(lp,src,sizeof(unsigned long*)*numkeys);
+            //第一个字符串直接复制过去就可以了
+            //长度是全部字符串的最小长度
             memcpy(res,src[0],minlen);
 
             /* Different branches per different operations for speed (sorry). */
             if (op == BITOP_AND) {
+                //与操作
                 while(minlen >= sizeof(unsigned long)*4) {
+                    //遍历每一个字符，进行与操作
                     for (i = 1; i < numkeys; i++) {
                         lres[0] &= lp[i][0];
                         lres[1] &= lp[i][1];
@@ -395,12 +466,15 @@ void bitopCommand(client *c) {
                         lres[3] &= lp[i][3];
                         lp[i]+=4;
                     }
+                    //累加偏移
                     lres+=4;
                     j += sizeof(unsigned long)*4;
                     minlen -= sizeof(unsigned long)*4;
                 }
             } else if (op == BITOP_OR) {
+                //或操作
                 while(minlen >= sizeof(unsigned long)*4) {
+                    //循环进行遍历，对每一个进行或操作
                     for (i = 1; i < numkeys; i++) {
                         lres[0] |= lp[i][0];
                         lres[1] |= lp[i][1];
@@ -413,7 +487,9 @@ void bitopCommand(client *c) {
                     minlen -= sizeof(unsigned long)*4;
                 }
             } else if (op == BITOP_XOR) {
+                //异或操作
                 while(minlen >= sizeof(unsigned long)*4) {
+                    //循环遍历
                     for (i = 1; i < numkeys; i++) {
                         lres[0] ^= lp[i][0];
                         lres[1] ^= lp[i][1];
@@ -426,6 +502,7 @@ void bitopCommand(client *c) {
                     minlen -= sizeof(unsigned long)*4;
                 }
             } else if (op == BITOP_NOT) {
+                //非操作，自己取反就可以了
                 while(minlen >= sizeof(unsigned long)*4) {
                     lres[0] = ~lres[0];
                     lres[1] = ~lres[1];
@@ -439,39 +516,53 @@ void bitopCommand(client *c) {
         }
 
         /* j is set to the next byte to process by the previous loop. */
+        //剩余的字符串再进行处理
         for (; j < maxlen; j++) {
             output = (len[0] <= j) ? 0 : src[0][j];
+            //对于非操作，剩余的也是直接取反
             if (op == BITOP_NOT) output = ~output;
             for (i = 1; i < numkeys; i++) {
+                //如果超出字符串长度，就取值[0]，否则就去字符串的字符
                 byte = (len[i] <= j) ? 0 : src[i][j];
                 switch(op) {
+                //与操作
                 case BITOP_AND: output &= byte; break;
+                //或操作
                 case BITOP_OR:  output |= byte; break;
+                //异或操作
                 case BITOP_XOR: output ^= byte; break;
                 }
             }
             res[j] = output;
         }
     }
+    //降低redis数据的引用
     for (j = 0; j < numkeys; j++) {
         if (objects[j])
             decrRefCount(objects[j]);
     }
+    //释放资源
     zfree(src);
     zfree(len);
     zfree(objects);
 
     /* Store the computed value into the target key */
+    //存储计算得到的结果
     if (maxlen) {
+        //计算得到了结果，就需要生成一个redis数据，并存储
         o = createObject(OBJ_STRING,res);
+        //存储起来
         setKey(c->db,targetkey,o);
         notifyKeyspaceEvent(NOTIFY_STRING,"set",targetkey,c->db->id);
         decrRefCount(o);
     } else if (dbDelete(c->db,targetkey)) {
+        //[maxlen]==[0]标示输入的全部redis数据都是空或不存在的
+        //所以需要删除[targetkey]标识的数据
         signalModifiedKey(c->db,targetkey);
         notifyKeyspaceEvent(NOTIFY_GENERIC,"del",targetkey,c->db->id);
     }
     server.dirty++;
+    //返回计算后得到的数据字符串长度
     addReplyLongLong(c,maxlen); /* Return the output string length in bytes. */
 }
 
